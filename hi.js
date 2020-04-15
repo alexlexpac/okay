49f49ac18115cb2e14de185c1245c5ec2602dfa7
alexlexpac <63712979+alexlexpac@users.noreply.github.com>
2020-04-15 16:11:16 +0530

    Update hi.js

--- hi.js
+++ hi.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-console.log("print of emit is not clear enough")
-=======
-
-
+//You’re receiving this email because you recently created a new GitHub account or added a new email address. If this wasn’t you, please ignore this email.
